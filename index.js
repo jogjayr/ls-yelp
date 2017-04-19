0dbcf8e5f3d7eaa61c18728ded7186a1f01927bf
Jayraj Jog <jayraj@skyhighnetworks.com>
2017-04-18 22:23:45 -0700

    kinda working

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+#! /usr/bin/env node
+const program = require('commander');
 const Yelp = require('node-yelp-api-v3');
 const keys = require('./keys');
+
+
+function list(val) {
+  return val.split(',');
+}
+
+program
+  .version('1.0.0')
+  .option('-c, --cuisine <items>', 'Comma-separated list of cuisines. Eg. vietnamese,vegan', list)
+  .parse(process.argv);
+ 
 const yelp = new Yelp({
   consumer_key: keys.appId,
   consumer_secret: keys.appSecret
 });
-yelp.getBusinessById('yelp-san-francisco').then((result) => console.log(result));
+if (program.cuisine) {
+  const params = { 
+    categories: program.cuisine.join(','),
+    location: '94040' 
+  };
+  yelp.searchBusiness(params).then((result) => console.log(result));
+}
